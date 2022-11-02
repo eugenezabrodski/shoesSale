66a356f948ed1b24bbdd3710920a533436a571bf
eugenezabrodski <115879896+eugenezabrodski@users.noreply.github.com>
2022-11-02 15:13:55 +0300

    project created

---
+++ shoesSale/shoesSale/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  shoesSale
+//
+//  Created by Евгений Забродский on 2.11.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
